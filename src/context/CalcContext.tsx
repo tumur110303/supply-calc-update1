5b8cc3d322e570963183c1351420c4ec386a1439
tumuruu <mbt.isee@gmail.com>
2023-02-11 18:29:00 +0800

    jijig 2.11

--- src/context/CalcContext.tsx
+++ src/context/CalcContext.tsx
@@ -33,7 +33,7 @@ type Conductor = (
   conductorType: "CC" | "CW" | "AC" | "AW",
   earthType: "TT" | "TN-S",
   onePhase?: boolean
-) => string;
+) => string | object;
 type GetLargeValue = (value: number, arr: number[]) => number;
 type GiveConductorArr = (
   conductorType: "CC" | "CW" | "AC" | "AW",
@@ -213,12 +213,599 @@ export const CalcStore: FC = ({ children }) => {
     earthType,
     onePhase
   ) => {
-    const objArr: {}[] = [
-      ...giveConductorArr(conductorType, earthType, onePhase),
+    // 220B ...
+    const aluminumCable220 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "3x2.5мм.кв" : "2x2.5мм.кв",
+        allowCurrent: earthType === "TN-S" ? 19 : 21,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "3x4мм.кв" : "2x4мм.кв",
+        allowCurrent: earthType === "TN-S" ? 27 : 29,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "3x6мм.кв" : "2x6мм.кв",
+        allowCurrent: earthType === "TN-S" ? 32 : 38,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "3x10мм.кв" : "2x10мм.кв",
+        allowCurrent: earthType === "TN-S" ? 42 : 55,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "3x16мм.кв" : "2x16мм.кв",
+        allowCurrent: earthType === "TN-S" ? 60 : 70,
+      },
+      {
+        section: 25,
+        text: earthType === "TN-S" ? "3x25мм.кв" : "2x25мм.кв",
+        allowCurrent: earthType === "TN-S" ? 75 : 90,
+      },
+      {
+        section: 35,
+        text: earthType === "TN-S" ? "3x35мм.кв" : "2x35мм.кв",
+        allowCurrent: earthType === "TN-S" ? 90 : 105,
+      },
+      {
+        section: 50,
+        text: earthType === "TN-S" ? "3x50мм.кв" : "2x50мм.кв",
+        allowCurrent: earthType === "TN-S" ? 110 : 135,
+      },
+      {
+        section: 70,
+        text: earthType === "TN-S" ? "3x70мм.кв" : "2x70мм.кв",
+        allowCurrent: earthType === "TN-S" ? 140 : 165,
+      },
+      {
+        section: 95,
+        text: earthType === "TN-S" ? "3x95мм.кв" : "2x95мм.кв",
+        allowCurrent: earthType === "TN-S" ? 170 : 200,
+      },
+      {
+        section: 120,
+        text: earthType === "TN-S" ? "3x120мм.кв" : "2x120мм.кв",
+        allowCurrent: earthType === "TN-S" ? 200 : 230,
+      },
+      {
+        section: 150,
+        text: earthType === "TN-S" ? "3x150мм.кв" : "2x150мм.кв",
+        allowCurrent: earthType === "TN-S" ? 235 : 270,
+      },
     ];
 
+    const aluminumWire220 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "3x(1x2.5)мм.кв" : "2x(1x2.5)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 19 : 20,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "3x(1x4)мм.кв" : "2x(1x4)мм.кв",
+        allowCurrent: 28,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "3x(1x6)мм.кв" : "2x(1x6)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 32 : 36,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "3x(1x10)мм.кв" : "2x(1x10)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 47 : 50,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "3x(1x16)мм.кв" : "2x(1x16)мм.кв",
+        allowCurrent: 60,
+      },
+      {
+        section: 25,
+        text: earthType === "TN-S" ? "3x(1x25)мм.кв" : "2x(1x25)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 80 : 85,
+      },
+      {
+        section: 35,
+        text: earthType === "TN-S" ? "3x(1x35)мм.кв" : "2x(1x35)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 95 : 100,
+      },
+      {
+        section: 50,
+        text: earthType === "TN-S" ? "3x(1x50)мм.кв" : "2x(1x50)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 130 : 140,
+      },
+      {
+        section: 70,
+        text: earthType === "TN-S" ? "3x(1x70)мм.кв" : "2x(1x70)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 165 : 175,
+      },
+      {
+        section: 95,
+        text: earthType === "TN-S" ? "3x(1x95)мм.кв" : "2x(1x95)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 200 : 215,
+      },
+      {
+        section: 120,
+        text: earthType === "TN-S" ? "3x(1x120)мм.кв" : "2x(1x120)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 220 : 245,
+      },
+      {
+        section: 150,
+        text: earthType === "TN-S" ? "3x(1x150)мм.кв" : "2x(1x150)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 255 : 275,
+      },
+    ];
+
+    const copperCable220 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "3x2.5мм.кв" : "2x2.5мм.кв",
+        allowCurrent: earthType === "TN-S" ? 25 : 27,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "3x4мм.кв" : "2x4мм.кв",
+        allowCurrent: earthType === "TN-S" ? 35 : 38,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "3x6мм.кв" : "2x6мм.кв",
+        allowCurrent: earthType === "TN-S" ? 42 : 50,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "3x10мм.кв" : "2x10мм.кв",
+        allowCurrent: earthType === "TN-S" ? 55 : 70,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "3x16мм.кв" : "2x16мм.кв",
+        allowCurrent: earthType === "TN-S" ? 75 : 90,
+      },
+      {
+        section: 25,
+        text: earthType === "TN-S" ? "3x25мм.кв" : "2x25мм.кв",
+        allowCurrent: earthType === "TN-S" ? 95 : 115,
+      },
+      {
+        section: 35,
+        text: earthType === "TN-S" ? "3x35мм.кв" : "2x35мм.кв",
+        allowCurrent: earthType === "TN-S" ? 120 : 140,
+      },
+      {
+        section: 50,
+        text: earthType === "TN-S" ? "3x50мм.кв" : "2x50мм.кв",
+        allowCurrent: earthType === "TN-S" ? 145 : 175,
+      },
+      {
+        section: 70,
+        text: earthType === "TN-S" ? "3x70мм.кв" : "2x70мм.кв",
+        allowCurrent: earthType === "TN-S" ? 180 : 215,
+      },
+      {
+        section: 95,
+        text: earthType === "TN-S" ? "3x95мм.кв" : "2x95мм.кв",
+        allowCurrent: earthType === "TN-S" ? 220 : 260,
+      },
+      {
+        section: 120,
+        text: earthType === "TN-S" ? "3x120мм.кв" : "2x120мм.кв",
+        allowCurrent: earthType === "TN-S" ? 300 : 260,
+      },
+      {
+        section: 150,
+        text: earthType === "TN-S" ? "3x150мм.кв" : "2x150мм.кв",
+        allowCurrent: earthType === "TN-S" ? 305 : 350,
+      },
+    ];
+
+    const copperWire220 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "3x(1x2.5)мм.кв" : "2x(1x2.5)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 25 : 27,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "3x(1x4)мм.кв" : "2x(1x4)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 35 : 38,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "3x(1x6)мм.кв" : "2x(1x6)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 42 : 46,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "3x(1x10)мм.кв" : "2x(1x10)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 60 : 70,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "3x(1x16)мм.кв" : "2x(1x16)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 80 : 85,
+      },
+      {
+        section: 25,
+        text: earthType === "TN-S" ? "3x(1x25)мм.кв" : "2x(1x25)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 100 : 115,
+      },
+      {
+        section: 35,
+        text: earthType === "TN-S" ? "3x(1x35)мм.кв" : "2x(1x35)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 125 : 135,
+      },
+      {
+        section: 50,
+        text: earthType === "TN-S" ? "3x(1x50)мм.кв" : "2x(1x50)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 170 : 185,
+      },
+      {
+        section: 70,
+        text: earthType === "TN-S" ? "3x(1x70)мм.кв" : "2x(1x70)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 210 : 225,
+      },
+      {
+        section: 95,
+        text: earthType === "TN-S" ? "3x(1x95)мм.кв" : "2x(1x95)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 255 : 275,
+      },
+      {
+        section: 120,
+        text: earthType === "TN-S" ? "3x(1x120)мм.кв" : "2x(1x120)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 290 : 315,
+      },
+      {
+        section: 150,
+        text: earthType === "TN-S" ? "3x(1x150)мм.кв" : "2x(1x150)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 330 : 360,
+      },
+    ];
+
+    // 380B ...
+    const aluminumCable380 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "5x2.5мм.кв" : "4x2.5мм.кв",
+        allowCurrent: earthType === "TN-S" ? 17 : 17,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "5x4мм.кв" : "4x4мм.кв",
+        allowCurrent: earthType === "TN-S" ? 24 : 24,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "5x6мм.кв" : "4x6мм.кв",
+        allowCurrent: earthType === "TN-S" ? 29 : 29,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "5x10мм.кв" : "4x10мм.кв",
+        allowCurrent: earthType === "TN-S" ? 38 : 38,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "5x16мм.кв" : "4x16мм.кв",
+        allowCurrent: earthType === "TN-S" ? 54 : 54,
+      },
+      {
+        section: 25,
+        text: earthType === "TN-S" ? "4x25+1x16мм.кв" : "3x25+1x16мм.кв",
+        allowCurrent: earthType === "TN-S" ? 68 : 68,
+      },
+      {
+        section: 35,
+        text: earthType === "TN-S" ? "4x35+1x25мм.кв" : "3x35+1x25мм.кв",
+        allowCurrent: earthType === "TN-S" ? 81 : 81,
+      },
+      {
+        section: 50,
+        text: earthType === "TN-S" ? "4x50+1x25мм.кв" : "3x50+1x25мм.кв",
+        allowCurrent: earthType === "TN-S" ? 100 : 100,
+      },
+      {
+        section: 70,
+        text: earthType === "TN-S" ? "4x70+1x35мм.кв" : "3x70+1x35мм.кв",
+        allowCurrent: earthType === "TN-S" ? 126 : 126,
+      },
+      {
+        section: 95,
+        text: earthType === "TN-S" ? "4x95+1x50мм.кв" : "3x95+1x50мм.кв",
+        allowCurrent: earthType === "TN-S" ? 153 : 153,
+      },
+      {
+        section: 120,
+        text: earthType === "TN-S" ? "4x120+1x70мм.кв" : "3x120+1x70мм.кв",
+        allowCurrent: earthType === "TN-S" ? 190 : 190,
+      },
+      {
+        section: 150,
+        text: earthType === "TN-S" ? "4x150+1x95мм.кв" : "3x150+1x95мм.кв",
+        allowCurrent: earthType === "TN-S" ? 212 : 212,
+      },
+      {
+        section: 185,
+        text: earthType === "TN-S" ? "4x185+1x95мм.кв" : "3x185+1x95мм.кв",
+        allowCurrent: earthType === "TN-S" ? 241 : 241,
+      },
+      {
+        section: 240,
+        text: earthType === "TN-S" ? "4x240+1x120мм.кв" : "3x240+1x120мм.кв",
+        allowCurrent: earthType === "TN-S" ? 274 : 274,
+      },
+    ];
+
+    const copperCable380 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "5x2.5мм.кв" : "4x2.5мм.кв",
+        allowCurrent: earthType === "TN-S" ? 22 : 22,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "5x4мм.кв" : "4x4мм.кв",
+        allowCurrent: earthType === "TN-S" ? 31 : 31,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "5x6мм.кв" : "4x6мм.кв",
+        allowCurrent: earthType === "TN-S" ? 38 : 38,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "5x10мм.кв" : "4x10мм.кв",
+        allowCurrent: earthType === "TN-S" ? 50 : 50,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "5x16мм.кв" : "4x16мм.кв",
+        allowCurrent: earthType === "TN-S" ? 68 : 68,
+      },
+      {
+        section: 25,
+        text: earthType === "TN-S" ? "4x25+1x16мм.кв" : "3x25+1x16мм.кв",
+        allowCurrent: earthType === "TN-S" ? 85 : 85,
+      },
+      {
+        section: 35,
+        text: earthType === "TN-S" ? "4x35+1x25мм.кв" : "3x35+1x25мм.кв",
+        allowCurrent: earthType === "TN-S" ? 108 : 108,
+      },
+      {
+        section: 50,
+        text: earthType === "TN-S" ? "4x50+1x25мм.кв" : "3x50+1x25мм.кв",
+        allowCurrent: earthType === "TN-S" ? 130 : 130,
+      },
+      {
+        section: 70,
+        text: earthType === "TN-S" ? "4x70+1x35мм.кв" : "3x70+1x35мм.кв",
+        allowCurrent: earthType === "TN-S" ? 162 : 162,
+      },
+      {
+        section: 95,
+        text: earthType === "TN-S" ? "4x95+1x50мм.кв" : "3x95+1x50мм.кв",
+        allowCurrent: earthType === "TN-S" ? 200 : 200,
+      },
+      {
+        section: 120,
+        text: earthType === "TN-S" ? "4x120+1x70мм.кв" : "3x120+1x70мм.кв",
+        allowCurrent: earthType === "TN-S" ? 234 : 234,
+      },
+      {
+        section: 150,
+        text: earthType === "TN-S" ? "4x150+1x95мм.кв" : "3x150+1x95мм.кв",
+        allowCurrent: earthType === "TN-S" ? 275 : 275,
+      },
+      {
+        section: 185,
+        text: earthType === "TN-S" ? "4x185+1x95мм.кв" : "3x185+1x95мм.кв",
+        allowCurrent: earthType === "TN-S" ? 308 : 308,
+      },
+      {
+        section: 240,
+        text: earthType === "TN-S" ? "4x240+1x120мм.кв" : "3x240+1x120мм.кв",
+        allowCurrent: earthType === "TN-S" ? 355 : 355,
+      },
+    ];
+
+    const aluminumWire380 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "5x(1x2.5)мм.кв" : "4x(1x2.5)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 15 : 19,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "5x(1x4)мм.кв" : "4x(1x4)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 22 : 23,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "5x(1x6)мм.кв" : "4x(1x6)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 26 : 30,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "5x(1x10)мм.кв" : "4x(1x10)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 38 : 39,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "5x(1x16)мм.кв" : "4x(1x16)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 48 : 55,
+      },
+      {
+        section: 25,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x25)+(1x16)мм.кв"
+            : "3x(1x25)+(1x16)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 65 : 70,
+      },
+      {
+        section: 35,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x35)+(1x25)мм.кв"
+            : "3x(1x35)+(1x25)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 75 : 85,
+      },
+      {
+        section: 50,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x50)+(1x25)мм.кв"
+            : "3x(1x50)+(1x25)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 105 : 120,
+      },
+      {
+        section: 70,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x70)+(1x35)мм.кв"
+            : "3x(1x70)+(1x35)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 130 : 140,
+      },
+      {
+        section: 95,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x95)+(1x50)мм.кв"
+            : "3x(1x95)+(1x50)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 175 : 175,
+      },
+      {
+        section: 120,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x120)+(1x70)мм.кв"
+            : "3x(1x120)+(1x70)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 200 : 200,
+      },
+    ];
+
+    const copperWire380 = [
+      {
+        section: 2.5,
+        text: earthType === "TN-S" ? "5x(1x2.5)мм.кв" : "4x(1x2.5)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 20 : 25,
+      },
+      {
+        section: 4,
+        text: earthType === "TN-S" ? "5x(1x4)мм.кв" : "4x(1x4)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 28 : 30,
+      },
+      {
+        section: 6,
+        text: earthType === "TN-S" ? "5x(1x6)мм.кв" : "4x(1x6)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 34 : 40,
+      },
+      {
+        section: 10,
+        text: earthType === "TN-S" ? "5x(1x10)мм.кв" : "4x(1x10)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 48 : 50,
+      },
+      {
+        section: 16,
+        text: earthType === "TN-S" ? "5x(1x16)мм.кв" : "4x(1x16)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 64 : 75,
+      },
+      {
+        section: 25,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x25)+(1x16)мм.кв"
+            : "3x(1x25)+(1x16)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 80 : 90,
+      },
+      {
+        section: 35,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x35)+(1x25)мм.кв"
+            : "3x(1x35)+(1x25)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 100 : 115,
+      },
+      {
+        section: 50,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x50)+(1x25)мм.кв"
+            : "3x(1x50)+(1x25)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 135 : 150,
+      },
+      {
+        section: 70,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x70)+(1x35)мм.кв"
+            : "3x(1x70)+(1x35)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 165 : 185,
+      },
+      {
+        section: 95,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x95)+(1x50)мм.кв"
+            : "3x(1x95)+(1x50)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 255 : 255,
+      },
+      {
+        section: 120,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x120)+(1x70)мм.кв"
+            : "3x(1x120)+(1x70)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 260 : 260,
+      },
+      {
+        section: 150,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x150)+(1x95)мм.кв"
+            : "3x(1x150)+(1x95)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 300 : 300,
+      },
+      {
+        section: 185,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x185)+(1x95)мм.кв"
+            : "3x(1x185)+(1x95)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 346 : 346,
+      },
+      {
+        section: 240,
+        text:
+          earthType === "TN-S"
+            ? "4x(1x240)+(1x120)мм.кв"
+            : "3x(1x240)+(1x120)мм.кв",
+        allowCurrent: earthType === "TN-S" ? 397 : 397,
+      },
+    ];
+
+    const objArr = (
+      currentCB: number,
+      arr: { section: number; text: string; allowCurrent: number }[]
+    ) => {
+      console.log("Обьект буцаах функц...");
+      for (const e of arr) {
+        if (e.allowCurrent < currentCB) continue;
+        return e;
+      }
+    };
+
+    let value = objArr(circuitBreakerCurrent, copperCable380);
+
     // const selectedConductor = getLargeValue(circuitBreakerCurrent, table)
-    return "3х2.5мм.кв";
+    return value
+      ? value
+      : "Нэг хэлхээгээр дамжуулахад ачаалал харьцангуй их байгаа тул, ачааллыг хоёр хувааж дамжуулахаар төлөвлөх нь илүү тохиромжтой!";
   };
 
   // ########################### ТУСЛАХ ФУНКЦУУД  ################################
